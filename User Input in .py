f13b1f01feba055ed8b9a3cf1e1f7ed0482b9a4f
YADAV SHUBHAM JANGBAHADUR <153888821+shubh0022@users.noreply.github.com>
2024-12-14 19:33:31 +0530

    Create User Input in .py

---
+++ User Input in .py
@@ -0,0 +1,22 @@
+
+a= int(input("enter the value 1:"))
+b= int(input("enter the value 2:"))
+def add(a,b):
+   return a+b
+print("the sum is :", add(a,b))
+
+def sub(a,b):
+   return a-b
+print("the sum is :", sub(a,b))
+
+def multi(a,b):
+   return a*b
+print("the multi is :", multi(a,b)) 
+def div(a,b):
+   return a/b
+print("the div is :", div(a,b))
+def rem(a,b):
+   return a+b
+print("the rem is :", rem(a,b))
+
+
