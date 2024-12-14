56fdab316bfd83efdd30dbc018f419c75a1cddac
YADAV SHUBHAM JANGBAHADUR <153888821+shubh0022@users.noreply.github.com>
2024-12-14 19:35:50 +0530

    Create Area and perimeter.py

---
+++ Area and perimeter.py
@@ -0,0 +1,11 @@
+a= int(input("enter  the length of  a="))
+
+b= int(input("enter  the breath of b="))
+def perimeter(a,b):
+   return a*b
+   
+   print (" the area of  square",area(a,b))
+   
+def perimeter(a,b):
+      return 2*a*b
+print (" the area of  square",perimeter(a,b))
